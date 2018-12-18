98f6f25093c7cd76964c102cfa129678ed5dfce6
zzh9504 <zzh9504@outlook.com>
2018-12-18 21:36:04 +0800

    加载中移动端处理完成

--- public/m/js/cart.js
+++ public/m/js/cart.js
@@ -1,5 +1,5 @@
 $(function() {
-
+   
     // 调用封装好的查询购车商品和渲染函数
     queryCart();
 
@@ -45,9 +45,10 @@ $(function() {
                                 } else {
                                     // []  一开始这个样子的数组
                                     // {
-                                    // 	data:[]
+                                    //  data:[]
                                     // }// 变成这个样子的对象
                                     console.log(data instanceof Array);
+                                    
                                     // 判断后返回的数据是不是一个数组 是一个数组 转成一个对象 给对象添加一个data数组 值就是当前的data
                                     if (data instanceof Array) {
                                         data = {
@@ -61,6 +62,7 @@ $(function() {
                                         $('.cart-list').append(html);
                                         // 9. 结束上拉加载效果
                                         mui('#refreshContainer').pullRefresh().endPullupToRefresh(false);
+                                        
                                     } else {
                                         // 10. 结束上拉加载效果提示没有数据了
                                         mui('#refreshContainer').pullRefresh().endPullupToRefresh(true);
@@ -74,18 +76,143 @@ $(function() {
             }
         }
     });
+
+  
+    // 1. 给所有复选框添加 change事件 
+    $('.cart-list').on('change', '.choose', function() {
+        // 2. 获取所有选中的复选框
+        var checkeds = $('.choose:checked');
+        console.log(checkeds);
+        // 所有商品总价
+        var sum = 0;
+        // 3. 遍历所有选中的复选框 计算每个商品单价
+        checkeds.each(function(index, value) {
+            // 4. 获取当前复选框的价格和 数量
+            var price = $(value).data('price');
+            var num = $(value).data('num');
+            var count = price * num;
+            sum += count;
+        });
+        // 让当前sum保留2位小数
+        // 0.034 * 100 3/100  == 0.03
+        // sum = parseInt(sum * 100) / 100;
+        sum = sum.toFixed(2);
+        $('.order-total span').html(sum);
+    });
+
+   
+    // 1. 委托给删除按钮添加点击事件
+    $('.cart-list').on('tap', '.btn-delete', function() {
+        // 当前点击a元素
+        var that = this;
+        // 2. 弹出一个确认框
+        mui.confirm('您真的要删除我吗?', '温馨提示', ['确定', '取消'], function(e) {
+            // 点击确定或者取消 会触发回调函数 并且 e.index == 0 确定 e.index == 1取消
+            // 3. 判断点击确定还是取消
+            if (e.index == 0) {
+                // 4. 点击了确定 拿到当前删除购物车id
+                var id = $(that).data('id');
+                // 5. 调用API删除
+                $.ajax({
+                    url: '/cart/deleteCart',
+                    data: { id: id },
+                    success: function(data) {
+                        // 6. 判断如果删除成功就重新刷新页面
+                        if (data.success) {
+                            
+                            // 7. 调用查询购物车商品的函数
+                            queryCart();
+                        }
+                    }
+                })
+            } else if (e.index == 1) {
+                // 8. 把侧滑列表滑动回去
+                console.log(this);
+
+                // 8.3 使用MUI官方提供函数 是MUI调用的  而且参数是a的父元素的父元素  必须的DOM对象
+                mui.swipeoutClose($(that).parent().parent()[0]);
+            }
+        });
+    });
+
+   
+    // 1. 给编辑按钮添加点击事件
+    $('.cart-list').on('tap', '.btn-edit', function() {
+        var that = this;
+        // 2. 拿到当前编辑商品的对象
+        var product = $(this).data('product');
+        // 3. 尺码是字符串 30-50 处理成数组
+        var min = product.productSize.split('-')[0] - 0;
+        var max = product.productSize.split('-')[1];
+        product.productSize = [];
+        for (var i = min; i <= max; i++) {
+            product.productSize.push(i);
+        }
+        console.log(product);
+        // 4. 调用模板 生成html  传入当前的商品对象 拿当前商品对象去渲染模板
+        var html = template('editCartTpl', product);
+        console.log(html);
+        // 5. 使用正则去掉字符串里面的回车换行
+        html = html.replace(/[\r\n]/g, "");
+        console.log(html);
+        // 因为html字符串里面有很多回车换行 到了确认框里面会变成br标签 在放进去之前要把回车换行去掉
+        // 6. 把准备的好的编辑模板放到确认框里面
+        mui.confirm(html, '编辑商品', ['确定', '取消'], function(e) {
+            // 8. 判断点击确定还是取消
+            if (e.index == 0) {
+                // 9. 点击确定 拿到最新改的尺码数量 调用APi传参编辑
+                $.ajax({
+                    url: '/cart/updateCart',
+                    type: 'post',
+                    data: {
+                        id: product.id, // 当前购物车id
+                        size: $('.btn-size.active').data('size'), // 当前选择尺码
+                        num: mui('.mui-numbox').numbox().getValue() //当前选择数量
+                    },
+                    success: function(data) {
+                        // 10. 编辑成功就调用查询购物车刷新页面
+                        if (data.success) {
+                            queryCart();
+                        }
+                    }
+                });
+            } else {
+                // 10. 点击取消 把侧滑列表滑动回去
+                mui.swipeoutClose($(that).parent().parent()[0]);
+            }
+        });
+        // 7. 等到代码放到确认框后再初始化 尺码 和 数量
+        // 7.1 数字框也是动态添加要手动初始化  但这个时候只是能点击 还没有值
+        // mui('.mui-numbox').numbox()
+        // 7.2 初始化的时候 默认给赋值为当前选择的数量
+        mui('.mui-numbox').numbox().setValue(product.num);
+        // 7.3 尺码默认也是不能点击的手动初始化
+        $('.btn-size').on('tap', function() {
+            $(this).addClass('active').siblings().removeClass('active');
+        });
+    });
+
+
     // 把请求购物车商品数据函数封装起来
     function queryCart() {
         $.ajax({
             url: '/cart/queryCartPaging',
             data: { page: 1, pageSize: 4 },
+            beforeSend: function() { // 请求之前会触发的回调函数
+                // 请求之前显示遮罩层
+                $('.mask').show();
+            },
+            complete: function() { // 请求之后会触发函数
+                // 请求之后隐藏遮罩层
+                $('.mask').hide();
+            },
             success: function(data) {
                 // 判断当前返回数据是否报错 报错表示未登录 跳转到登录页面
                 if (data.error) {
                     // 跳转到登录页面 同时登录成功回到当前购物车页面
                     location = 'login.html?returnUrl=' + location.href;
                 } else {
-                	  // 注意页面刚刚加载请求数据 可能没有数据 也要把数据处理成一个对象 
+                    // 注意页面刚刚加载请求数据 可能没有数据 也要把数据处理成一个对象 
                     console.log(data instanceof Array);
                     // 判断后返回的数据是不是一个数组 是一个数组 转成一个对象 给对象添加一个data数组 值就是当前的data
                     if (data instanceof Array) {
@@ -95,6 +222,7 @@ $(function() {
                     }
                     console.log(data);
                     // 调用模板方法生成html
+                    
                     var html = template('cartListTpl', data);
                     // 渲染到购物车列表的容器里面
                     $('.cart-list').html(html);
--- public/m/js/productlist.js
+++ public/m/js/productlist.js
@@ -1,9 +1,5 @@
 $(function() {
-    /*1. 商品搜素
-    	1. 拿到搜索页面传递过来参数?search=鞋# 拿到参数中鞋
-    	2. 调用查询商品列表的API查询商品列表数据
-    	3. 创建商品列表的模板 传入数据
-    	4. 把模板渲染到页面*/
+    
     // 1. 获取url中的参数的值 按照=分割取第二个值(索引是1) 进行转码
     // 使用一个函数 根据参数名去参数的值
     var search = getQueryString('search');
@@ -11,12 +7,7 @@ $(function() {
     // 2. 调用查询商品数据的函数
     queryProduct();
     
-    /*2. 商品列表的商品搜索功能
-    		1. 点击搜索按钮获取当前输入的值  
-    		2. 做非空判断
-    		3. 调用API传入当前要搜索的商品关键字
-    		4. 接受返回的商品数据 调用模板渲染页面
-    */
+   
     // 1. 给当前搜索按钮添加点击事件
     $('.btn-search').on('tap', function() {
         // 2. 获取当前输入框的值  只要把外面的全局的search覆盖
@@ -30,6 +21,14 @@ $(function() {
         // 3. 根据当前输入的search发送请求渲染页面
         $.ajax({
             url: '/product/queryProduct',
+            beforeSend: function() { // 请求之前会触发的回调函数
+                // 请求之前显示遮罩层
+                $('.mask').show();
+            },
+            complete: function() { // 请求之后会触发函数
+                // 请求之后隐藏遮罩层
+                $('.mask').hide();
+            },
             // page 第几页 pageSize 每页大小 proName搜索关键字
             data: { page: 1, pageSize: 4, proName: search },
             success: function(data) {
@@ -41,15 +40,6 @@ $(function() {
         });
     }
 
-    /*3. 商品排序
-		1. 如何排序 调用API传入参数进行排序 如果价格传入price 数量传入num
-		2. 排序顺序 price=1升序 从小到大  price=2降序  从大到小
-		3. 点击了排序按钮后 如果现在是升序(1) 点击了后变成降序(2)
-		4. 在a标签默认存储一个排序顺序 默认1升序
-		5. 点击后切换这个排序顺序的属性的值
-		6. 还需要知道当前点击a排序方式是price还是num  获取a身上的排序方式
-		7. 调用APi传入 传入对应排序方式和排序顺序即可 后面渲染页面
-    */
     // 1. 给所有排序的a添加点击事件
     $('.product-list .title ul li a').on('tap', function() {
         // 2. 获取当前排序的顺序
@@ -60,15 +50,7 @@ $(function() {
         $(this).data('sort', sort);
         // 5. 获取当前排序的方式
         var sortType = $(this).data('sort-type');
-        // console.log(sortType);
-        // var obj = {
-        // 		sortType:1
-        // }
-        // 给对象添加一个属性的名称 这个名称是一个变量 你是要取变量里面的值作为对象的属性名称
-        // 不能用点 也不能直接在{}里面 只能使用[]添加
-        // obj.sortType = 2;
-        // obj[sortType] = 2;
-        // console.log(obj);
+        
         var params = { page: 1, pageSize: 4, proName: search};
         // 给对象动态添加键 和 值  键名是变量不能使用.不能直接在{}里面加 一定要使用[]
         params[sortType] = sort;
@@ -84,50 +66,10 @@ $(function() {
                 $('.product-list .content ul').html(html);
             }
         });
-        /* 
-	         if (sortType == 'price') {
-	            // 6. 调用API传入对应的参数
-	            $.ajax({
-	                url: '/product/queryProduct',
-	                // page 第几页 pageSize 每页大小 proName搜索关键字
-	                data: { page: 1, pageSize: 4, proName: search, price: sort },
-	                success: function(data) {
-	                    // 3. 调用模板生成html
-	                    var html = template('productListTpl', data);
-	                    // 4. 渲染到商品列表内容的ul
-	                    $('.product-list .content ul').html(html);
-	                }
-	            });
-	         } else if (sortType == 'num') {
-	            // 6. 调用API传入对应的参数
-	            $.ajax({
-	                url: '/product/queryProduct',
-	                // page 第几页 pageSize 每页大小 proName搜索关键字
-	                data: { page: 1, pageSize: 4, proName: search, num: sort },
-	                success: function(data) {
-	                    // 3. 调用模板生成html
-	                    var html = template('productListTpl', data);
-	                    // 4. 渲染到商品列表内容的ul
-	                    $('.product-list .content ul').html(html);
-	                }
-	            });
-	         }
-        */
+       
     });
 
-    /*4. 下拉刷新和上拉加载
-    	1. 调用MUI的初始化方法初始化下拉刷新和上拉加载效果
-    	2. 指定下拉刷新的回调函数 实现刷新数据
-    	3. 调用MUI结束下拉刷新的效果不然会一直转
-    	4. 指定上拉加载的回调函数加载更多数据
-    	5. 定义一个page当前页码数 每次上拉把page++ 请求下一页数据(更多的数据)
-    	6. 把数据渲染后追加到页面 使用append
-    	7. 追加完成结束上拉加载效果
-    	8. 判断当如果上拉没有数据的时候 要结束上拉并且提示没有数据了 传入一个true
-    	9. 但是再次下拉的时候需要能够重新上拉所以下拉完了后要重置上拉加载效果
-    	10. 而且page也要从头开始 重置为1
-    */
-
+ 
     // 4. 定义一个全局变量page表示当前页面数
     var page = 1;
     // 1. 初始化下拉刷新的效果
@@ -191,11 +133,7 @@ $(function() {
         }
     });
 
-    /*5. 点击立即购买跳转到商品详情去购买商品
-	    1. 给所有购买按钮添加点击事件
-	    2. 获取当前点击商品id 
-	    3. 使用location跳转商品详情页面  把id作为参数传递到商品详情
-	    */
+   
 	 // 1. 给所有购买按钮添加点击事件
 	 $('.product-list').on('tap','.btn-buy',function () {
 	 		// 2. 获取当前点击商品id 
@@ -215,39 +153,3 @@ $(function() {
         return null;
     }
 });
-// http://localhost:3000/m/productlist.html?search=%E9%9E%8B&price=1&num=2
-// 根据url中参数名获取值
-// ?search=%E9%9E%8B&price=1&num=2 参数
-// 参数名 = search  参数值 = 鞋
-// 参数名 = price  参数值 = 1
-// 参数名 = num  参数值 = 2
-// function getQueryString(name){
-// 	// console.log(location);
-// 	// console.log(location.search);
-// 	// 1. 获取整个url参数的值的字符串 去掉第一个字符串 按照&分割成一个数组
-// 	var urlParams = location.search.substr(1).split('&');
-// 	// var urlParams = location.search.substring(1,location.search.length);
-// 	// console.log(urlParams);
-// 	for (var i = 0; i < urlParams.length; i++) {
-// 		// search=%E9%9E%8B  =前面参数名 以=分割 如果等号前面的和参数名一样 返回=号后面的参数的值
-// 		if(name == urlParams[i].split('=')[0]){
-// 			// 返回当前参数的值 同时转码
-// 			return decodeURI(urlParams[i].split('=')[1]);
-// 		}
-// 	}
-// }
-// console.log(getQueryString('search'));//鞋
-// console.log(getQueryString('price'));//1
-// console.log(getQueryString('num'));//2
-
-
-// function getQueryString(name) {
-//     var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
-//     var r = window.location.search.substr(1).match(reg);
-//     console.log(r);
-//     if (r != null) {
-//         //转码方式改成 decodeURI
-//         return decodeURI(r[2]);
-//     }
-//     return null;
-// }
